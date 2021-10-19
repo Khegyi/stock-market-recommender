ceb341cf6523cb02e4a80d29239f5ff0ad79614a
Khegyi <khegyi.balint@gmail.com>
2021-10-19 18:47:32 +0200

    unit tests

---
+++ methods.js
@@ -0,0 +1,156 @@
+const parseDate = (str) => {
+  const ymd = str.split("-");
+  return new Date(ymd[0], ymd[1] - 1, ymd[2]);
+};
+
+function stringifyDate(date) {
+  return date.toISOString().substring(0, 10);
+}
+
+const stockPriceGenerator = (stckSymbol, priceDate) => {
+  const stckPrice = Math.floor(Math.random() * 100);
+  return {
+    stckDate: stringifyDate(priceDate),
+    price: stckPrice,
+    stockSymbol: stckSymbol,
+  };
+};
+const socialMediaCountGenerator = (stckSymbol, smType) => {
+  const smCount = Math.floor(Math.random() * 1000);
+  return { socialMediaType: smType, count: smCount, stockSymbol: stckSymbol };
+};
+const recommendationAlgorithm = (
+  stckPrice,
+  smCount,
+  algorithm,
+  constants,
+  risk_ratios
+) => {
+  let recommendation = "";
+
+  let algorithmConsts = {};
+
+  switch (algorithm) {
+    case "Algo1":
+      algorithmConsts = {
+        buyTHold: 80,
+        sellTHold: 30,
+        smCountTHold: 550,
+      };
+      break;
+    case "Algo2":
+      algorithmConsts = {
+        buyTHold: 60,
+        sellTHold: 45,
+        smCountTHold: 600,
+      };
+      break;
+    case "Algo3":
+      algorithmConsts = {
+        buyTHold: 75,
+        sellTHold: 25,
+        smCountTHold: 450,
+      };
+      break;
+
+    default:
+      algorithmConsts = {
+        buyTHold: 70,
+        sellTHold: 40,
+        smCountTHold: 500,
+      };
+      break;
+  }
+  if (smCount.count >= algorithmConsts.smCountTHold) {
+    if (algorithmConsts.buyTHold < stckPrice.price) recommendation = "sell";
+    if (algorithmConsts.sellTHold < stckPrice.price < algorithmConsts.buyTHold)
+      recommendation = "hold";
+    if (stckPrice.price < algorithmConsts.sellTHold) recommendation = "buy";
+  } else {
+    if (algorithmConsts.buyTHold < stckPrice.price) recommendation = "buy";
+    if (algorithmConsts.sellTHold < stckPrice.price < algorithmConsts.buyTHold)
+      recommendation = "hold";
+    if (stckPrice.price < algorithmConsts.sellTHold) recommendation = "sell";
+  }
+
+  return {
+    recommendation,
+    stockPrice: stckPrice.price,
+    recommendDate: stckPrice.stckDate,
+    smCount: smCount.count,
+  };
+};
+
+function displayRecommendations(resultList, stockSymbol) {
+  const resultListDiv = document.getElementById("results_table");
+  resultListDiv.innerHTML = "";
+  resultList.forEach((recommend) => {
+    let tr = document.createElement("tr");
+    tr.classList.add("recommendation");
+
+    const rec = document.createElement("td");
+    rec.textContent = recommend.recommendation;
+    rec.classList.add("rec", recommend.recommendation);
+
+    const smc = document.createElement("td");
+    smc.textContent = recommend.smCount;
+    smc.classList.add("smc");
+
+    const prc = document.createElement("td");
+    prc.textContent = recommend.stockPrice;
+    prc.classList.add("prc");
+
+    const date = document.createElement("td");
+    date.textContent = recommend.recommendDate;
+    date.classList.add("date");
+
+    tr.append(date);
+    tr.append(smc);
+    tr.append(prc);
+    tr.append(rec);
+
+    resultListDiv.append(tr);
+
+    document.getElementById("stock_symbol_title").textContent = stockSymbol;
+    const tableSection = document.getElementById("table");
+    if (tableSection.classList.contains("hidden")) {
+      tableSection.classList.remove("hidden");
+    }
+  });
+}
+
+const handleSubmit = () => {
+  const fromDateStr = document.getElementById("from_date").value;
+  const toDateStr = document.getElementById("to_date").value;
+  const socialMediaType = document.getElementById("social_media_type").value;
+  const stockSymbol = document.getElementById("stock_symbol").value;
+  const algorithm = document.getElementById("algorithm").value;
+  let recTable = [];
+
+  if (parseDate(fromDateStr) > parseDate(toDateStr)) {
+    alert("From Date cannot be later, than To Date");
+  } else {
+    for (
+      var date = parseDate(fromDateStr);
+      date <= parseDate(toDateStr);
+      date.setDate(date.getDate() + 1)
+    ) {
+      const recomandation = recommendationAlgorithm(
+        stockPriceGenerator(stockSymbol, date),
+        socialMediaCountGenerator(stockSymbol, socialMediaType),
+        algorithm
+      );
+      recTable.push(recomandation);
+    }
+    displayRecommendations(recTable, stockSymbol);
+  }
+};
+
+export {
+  displayRecommendations,
+  stringifyDate,
+  stockPriceGenerator,
+  socialMediaCountGenerator,
+  recommendationAlgorithm,
+  handleSubmit,
+};
--- test/scripts.test.js
+++ test/scripts.test.js
@@ -1,5 +1,64 @@
-import parseDate from "../scripts";
+import {
+  stringifyDate,
+  stockPriceGenerator,
+  socialMediaCountGenerator,
+  recommendationAlgorithm,
+} from "../methods";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(parseDate("2020-07-08")).toBe(new Date(2020, 07, 08));
+test("convert String to Date", () => {
+  const testResDate = new Date("2020-07-07T22:00:00.000Z");
+  expect(stringifyDate(testResDate)).toBe("2020-07-07");
+});
+
+test("stock PriceGenerator", () => {
+  const testResDate = new Date("2020-07-07T22:00:00.000Z");
+
+  const result = stockPriceGenerator("ASX", testResDate);
+  expect(result.price).toBeGreaterThanOrEqual(0);
+  expect(result.price).toBeLessThanOrEqual(100);
+  expect(result.stckDate).toBe("2020-07-07");
+  expect(result.stockSymbol).toBe("ASX");
+});
+
+test("social MediaCountGenerator ", () => {
+  const result = socialMediaCountGenerator("ASX", "Twitter");
+  expect(result.count).toBeGreaterThanOrEqual(0);
+  expect(result.count).toBeLessThanOrEqual(1000);
+  expect(result.socialMediaType).toBe("Twitter");
+  expect(result.stockSymbol).toBe("ASX");
+});
+test("recommendation Algorithm 1", () => {
+  const testResDate = new Date("2020-07-07T22:00:00.000Z");
+  const result = recommendationAlgorithm(
+    { price: 53, stckDate: testResDate, stockSymbol: "NASA" },
+    { socialMediaType: "Twitter", count: 632, stockSymbol: "NASA" },
+    "Algo1"
+  );
+  expect(result.recommendation).toBe("hold");
+  expect(result.stockPrice).toBe(53);
+  expect(result.smCount).toBe(632);
+});
+
+test("recommendation Algorithm 2", () => {
+  const testResDate = new Date("2020-07-07T22:00:00.000Z");
+  const result = recommendationAlgorithm(
+    { price: 83, stckDate: testResDate, stockSymbol: "NASA" },
+    { socialMediaType: "Twitter", count: 400, stockSymbol: "NASA" },
+    "Algo2"
+  );
+  expect(result.recommendation).toBe("hold");
+  expect(result.stockPrice).toBe(83);
+  expect(result.smCount).toBe(400);
+});
+
+test("recommendation Algorithm 3", () => {
+  const testResDate = new Date("2020-07-07T22:00:00.000Z");
+  const result = recommendationAlgorithm(
+    { price: 13, stckDate: testResDate, stockSymbol: "NASA" },
+    { socialMediaType: "Twitter", count: 900, stockSymbol: "NASA" },
+    "Algo3"
+  );
+  expect(result.recommendation).toBe("buy");
+  expect(result.stockPrice).toBe(13);
+  expect(result.smCount).toBe(900);
 });
